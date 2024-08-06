12f74d009c2b7d18ed97eb27e246ac5452b43f0c
Eterocell <eterocell@outlook.com>
2024-08-06 11:20:11 +0800

    release: 2.11.1-beta04
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,8 @@ subprojects {
             targetSdk = 34
             buildToolsVersion = "34.0.0"
 
-            versionName = "2.11.1-beta03"
-            versionCode = "02110130".toInt()
+            versionName = "2.11.1-beta04"
+            versionCode = "02110140".toInt()
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
